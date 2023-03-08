20e944533cc77e0286dc491c4c820d97e820d788
jonel <it.jonelfignacio@gmail.com>
2023-03-08 16:21:35 +0800

    add delete all trip by user id

--- routes/office/trip.js
+++ routes/office/trip.js
@@ -7,6 +7,7 @@ const isAuth = require("../../middleware/is-auth");
 router.get("/trips", isAuth, tripController.getTrips);
 router.put("/trip/:tripId", isAuth, tripController.updateTrip);
 router.delete("/trip/:tripId", isAuth, tripController.deleteTrip);
+router.delete("/trips/:userId", isAuth, tripController.deleteAllTrips);
 
 router.post("/apk-trip", isAuth, tripController.createApkTrip);
 router.get("/apk-trips", isAuth, tripController.getApkTrips);
